01be78e724a1c52efcdecb8102cd981057381c1e
Cruziken <bencruz@vols.utk.edu>
2018-05-29 08:53:46 -0400

    Update poetry.js

--- script/poetry.js
+++ script/poetry.js
@@ -1,9 +1,11 @@
-const password = 'lucario';
-var passwordTry = prompt("Please enter your name", "Your name here");
+//const password = 'lucario';
+//var passwordTry = prompt("Please enter your name", "Your name here");
 
-if (passwordTry == password) {
-    document.getElementById("content");
-}
-else {
-    document.getElementById("noacces");
-}
+//if (passwordTry == password) {
+    //document.getElementById("content");
+//}
+//else {
+  //  document.getElementById("noacces");
+//}
+ let d = new Date();
+      document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
