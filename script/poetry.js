464602c9f52ba755e87b52f7a7403a6c43e60ddf
Cruziken <bencruz@vols.utk.edu>
2018-05-29 08:42:05 -0400

    Create poetry.js

---
+++ script/poetry.js
@@ -0,0 +1,9 @@
+const password = 'lucario';
+var passwordTry = prompt("Please enter your name", "Your name here");
+
+if (passwordTry == lucario) {
+    document.getElementById("content");
+}
+else {
+    document.getElementById("noacces");
+}
